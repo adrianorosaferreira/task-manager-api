86763806f2f9c11b6b7f0e5332b6635a38111495
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-07-17 19:30:55 +0000

    Adding destroy action to the users controller with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -78,6 +78,7 @@ RSpec.describe 'Users API', type: :request do
             it 'returns status code 200' do
                 expect(response).to have_http_status(200)
             end
+
             it 'returns the json data for the update user' do
                 user_response = JSON.parse(response.body, symbolize_names: true)
                 expect(user_response[:email]).to eq(user_params[:email])
@@ -90,9 +91,29 @@ RSpec.describe 'Users API', type: :request do
             it 'returns status code 422' do
                 expect(response).to have_http_status(422)
             end
+            
             it 'returns the json data for the errors' do
                 user_response = JSON.parse(response.body, symbolize_names: true)
                 expect(user_response).to have_key(:errors)
+            end
+        end
+      end
+
+      describe 'DELETE /users/:id' do
+        before do
+            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+            delete "/users/#{user_id}", params: { }, headers: headers
+        end
+
+        context 'when the request params are valid' do
+            let(:user_params) { { email: 'new@taskmanager.com' } }
+
+            it 'returns status code 204' do
+                expect(response).to have_http_status(204)
+            end
+
+            it 'removes the user from database' do
+                expect( User.find_by(id: user.id) ).to be_nil
             end
         end
       end
