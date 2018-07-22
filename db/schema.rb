7b590a6fb015654fdc0096942cc428fe89460618
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-07-22 21:08:48 +0000

    Adding API::V2

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 20180721171424) do
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "description", limit: 65535
-    t.boolean  "done"
+    t.boolean  "done",                      default: false
     t.datetime "deadline"
     t.integer  "user_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
